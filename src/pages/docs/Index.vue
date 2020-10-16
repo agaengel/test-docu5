cfca0ee1281a6d1e7bdb3f5455e93444a70f7e1e
buildwith-ionos <>
2020-10-16 15:39:40 +0200

    update of "Getting Started"

--- src/pages/docs/Index.vue
+++ src/pages/docs/Index.vue
@@ -10,37 +10,43 @@
         </p>
       </div>
       <div class="content">
-        <h2>Getting started with Buildwith IONOS</h2>
+        <h2>Getting started with BuildWith IONOS</h2>
         <p>
           In this tutorial, we will show you how easy it is to launch your site
-          with Buildwith IONOS.
-          <a href="https://buildwith.ionos.com/">Login</a> with any existing
-          IONOS contract. If you are not already a IONOS user, go ahead and
-          <a href="https://www.ionos.com/">sign up</a> a contract.
+          with BuildWith IONOS.
         </p>
-        <p/>
-        <h2>Step 1: Start your first project</h2>
+        <h2>Step 1: Start</h2>
+        <p>
+          <button
+            class="text-white font-bold rounded-full bg-blue-600 inline-flex items-center shadow-lg focus:outline-none px-8 my-6 py-4"
+          >
+            <span>Drop to IONOS.space</span>
+          </button>
+          is probably the easiest way to start.
+        </p>
+        <p>
+          Or you <a href="https://buildwith.ionos.com/">login</a> the classic
+          way. The only thing you need is an existing IONOS contract. If you
+          have not already an IONOS contract, go ahead and
+          <a href="https://www.ionos.com/">sign up</a> for a new one.
+        </p>
+        <p />
         <picture class="w-full">
-          <source srcset="@images/setup-step1.webp" type="image/webp"/>
-          <source srcset="@images/setup-step1.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-01.webp" type="image/webp" />
+          <source srcset="@images/get-started-01.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step1.webp"
+            src="@images/get-started-01.webp"
             fit="contain"
-            alt="Screenshot of dashboard"
+            alt="Screenshot of BuildWith IONOS login"
           />
         </picture>
-        <p>
-          Creating a new site with Buildwith IONOS is simple. Once you've logged
-          in for the first time, click the <b>Start first project</b> button
-          shown above.
-        </p>
         <h2>Step 2: Connect to GitHub</h2>
-        <p>Clicking <b>Start first project</b> brings you to this screen:</p>
+        <p>BuildWith IONOS requisites your GitHub account:</p>
         <picture class="w-full">
-          <source srcset="@images/setup-step2.webp" teype="image/webp"/>
-          <source srcset="@images/setup-step2.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-02.webp" teype="image/webp" />
+          <source srcset="@images/get-started-02.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step2.webp"
+            src="@images/get-started-02.webp"
             fit="contain"
             alt="Screenshot of GitHub connect"
           />
@@ -49,48 +55,48 @@
           Click the <b>Connect to GitHub</b> button as illustrated in the
           screenshot above.
         </p>
-        <h2>Step 3: Authorize Buildwith IONOS</h2>
+        <h2>Step 3: Authorize BuildWith IONOS</h2>
         <picture class="w-full">
-          <source srcset="@images/setup-step3.webp" type="image/webp"/>
-          <source srcset="@images/setup-step3.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-03.webp" teype="image/webp" />
+          <source srcset="@images/get-started-03.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step3.webp"
+            src="@images/get-started-03.webp"
             fit="contain"
             alt="Screenshot of authorization"
           />
         </picture>
         <p>
-          It's time to allow Buildwith IONOS and GitHub to talk to each other.
+          It's time to allow BuildWith IONOS and GitHub to talk to each other.
           Clicking the Authorize Application button will do just that. Like it
-          says in the image above, Buildwith IONOS doesn't store your GitHub
+          says in the image above, BuildWith IONOS doesn't store your GitHub
           access token on our servers.
         </p>
+
         <h2>Step 4: Configure your project</h2>
         <picture class="w-full">
-          <source srcset="@images/setup-step4.webp" type="image/webp"/>
-          <source srcset="@images/setup-step4.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-04.webp" teype="image/webp" />
+          <source srcset="@images/get-started-04.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step4.webp"
+            src="@images/get-started-04.webp"
             fit="contain"
             alt="Screenshot of project configuration"
           />
         </picture>
         <p>
-          Take a starter to get familiar with Buildwith IONOS and create your
-          first live web project in seconds. For the first roundtrip we skip
-          advanced settings. Have a look at
-          <a href="_blank">Advanced Setup (coming soon)</a> for more details.
-          Click on <b>Continue</b> to initialize, build &amp; ship your first
-          web project on IONOS.
+          If you followed <i>Drop to IONOS.space</i> the source is automatically
+          given. But you're free to choose your own project. Or simply use one
+          of our starters to get familiar with BuildWith IONOS and create your
+          first live web project in seconds. Click on <b>Continue</b> to
+          initialize, build &amp; drop your first web project to IONOS.space.
         </p>
         <h2>
-          Step 5: Initialize, Build &amp; Ship your first project to IONOS
+          Step 5: Initialize, Build &amp; Drop your first project to IONOS
         </h2>
         <picture class="w-full">
-          <source srcset="@images/setup-step5-1.webp" type="image/webp"/>
-          <source srcset="@images/setup-step5-1.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-05-1.webp" teype="image/webp" />
+          <source srcset="@images/get-started-05-1.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step5-1.webp"
+            src="@images/get-started-05-1.webp"
             fit="contain"
             alt="Screenshot of project initialization"
           />
@@ -100,10 +106,10 @@
           on the project size this step lasts for longer.
         </p>
         <picture class="w-full">
-          <source srcset="@images/setup-step5-2.webp" type="image/webp"/>
-          <source srcset="@images/setup-step5-2.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-05-2.webp" teype="image/webp" />
+          <source srcset="@images/get-started-05-2.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step5-2.webp"
+            src="@images/get-started-05-2.webp"
             fit="contain"
             alt="Screenshot of project congrats"
           />
@@ -114,10 +120,10 @@
           of your project.
         </p>
         <picture class="w-full">
-          <source srcset="@images/setup-step5-3.webp" type="image/webp"/>
-          <source srcset="@images/setup-step5-3.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-05-3.webp" teype="image/webp" />
+          <source srcset="@images/get-started-05-3.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step5-3.webp"
+            src="@images/get-started-05-3.webp"
             fit="contain"
             alt="Screenshot of project details"
           />
@@ -127,7 +133,7 @@
           <a href="https://github.com/features/actions">GitHub Actions</a>
           should run automatically. You can check on GitHub directly or stay
           tuned on our detail page. Progress is indicated by <b>state</b> and a
-          thumbnail screenshot as soon as your project is successful shipped to
+          thumbnail screenshot as soon as your project is successful dropped to
           IONOS.
         </p>
         <h2>Step 6: All done</h2>
@@ -136,22 +142,17 @@
           of your newly published site:
         </p>
         <picture class="w-full">
-          <source srcset="@images/setup-step6.webp" type="image/webp"/>
-          <source srcset="@images/setup-step6.webp.png" type="image/png"/>
+          <source srcset="@images/get-started-06.webp" teype="image/webp" />
+          <source srcset="@images/get-started-06.webp.png" type="image/png" />
           <g-image
-            src="@images/setup-step6.webp"
+            src="@images/get-started-06.webp"
             fit="contain"
             alt="Screenshot of resulting web project"
           />
         </picture>
         <p>
-          Probably not the most outstanding website, but wasn't that easy?
-          Taking it a step further you should connect
-          <a href="_blank">set up your custom domain (coming soon)</a>.
-        </p>
-        <p>
-          Congratulations on your new website, and thanks for using Buildwith
-          IONOS!
+          Now, you should have a copy of this website on your own. Congrats, and
+          thanks for using BuildWith IONOS!
         </p>
       </div>
     </section>
@@ -161,7 +162,7 @@
 <script>
 export default {
   metaInfo: {
-    title: "Get started",
+    title: "Get started"
   }
-}
+};
 </script>
