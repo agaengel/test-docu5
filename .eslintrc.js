3d9522b7f3a5b2281c75ab03378d9f3fbfc1042b
Alexander Gängel <alexander.gaengel@ionos.com>
2020-10-22 13:40:57 +0200

    introduce prettier and lint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  parser: 'vue-eslint-parser',
+  plugins: ['gridsome'],
+  rules: {
+    'gridsome/format-query-block': 'error',
+    'gridsome/require-g-image-src': 'error',
+    'gridsome/require-g-link-to': 'error',
+  },
+}
