f8f67248818c59c411154a38caea107290237ed4
Alexander Gängel <alexander.gaengel@ionos.com>
2020-11-13 09:38:21 +0100

    use sampleCard in blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -8,51 +8,11 @@
       <div class="content">
         <div class="flex flex-wrap -mx-2">
           <div
-            class="w-full max-w-sm lg:w-1/3 mb-8 px-2"
+            class="w-full flex-none max-w-sm lg:w-1/3 mb-8 px-2"
             v-for="entry in $page.allBlog.edges"
             :key="entry.node.id"
           >
-            <!-- card view -->
-            <div class="bg-white overflow-hidden rounded-lg shadow-lg flex-1">
-              <!-- card header -->
-              <g-link
-                class="relative overflow-hidde items-stretch"
-                :to="
-                  entry.node.type === 'sample'
-                    ? entry.node.samplepath
-                    : entry.node.path
-                "
-              >
-                <picture class="flex-1">
-                  <g-image
-                    :alt="entry.node.image_caption"
-                    :src="entry.node.image"
-                  />
-                </picture>
-              </g-link>
-              <!-- card content -->
-              <div class="p-8">
-                <h2>
-                  <g-link
-                    class="block hover:text-pink-500"
-                    :to="
-                      entry.node.type === 'sample'
-                        ? entry.node.samplepath
-                        : entry.node.path
-                    "
-                    >{{ entry.node.type === 'sample' ? 'Sample: ' : ''
-                    }}{{ entry.node.title }}</g-link
-                  >
-                </h2>
-                <div class="flex flex-col text-base md:flex mb-4">
-                  <div>{{ entry.node.author.name }}</div>
-                  <div class="hidden md:block px-2"></div>
-                  <time :datetime="entry.node.datetime">{{
-                    entry.node.humanTime
-                  }}</time>
-                </div>
-              </div>
-            </div>
+            <SampleCard :item="entry" />
           </div>
         </div>
       </div>
@@ -61,9 +21,14 @@
 </template>
 
 <script>
+import SampleCard from '@/components/SampleCard'
+
 export default {
   metaInfo: {
     title: 'Blog',
+  },
+  components: {
+    SampleCard,
   },
 }
 </script>
@@ -85,6 +50,11 @@ query {
         }
         category {
           title
+        }
+        tags {
+          id
+          title
+          path
         }
         type
       }
